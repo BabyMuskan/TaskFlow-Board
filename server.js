91e317e11f17f52a91edbde226d07b4ec4fd50a6
Muskan <muskanjaved813@gmail.com>
2026-09-23 15:53:13 +0500

    Fix merge conflict in index.html

--- server.js
+++ server.js
@@ -72,9 +72,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
-    console.log('MongoDB Connected Successfully!');
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
